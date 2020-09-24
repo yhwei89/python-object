dc6d04037bde5be21a866bb71d2a6caa139a54f3
root <root@tclshop-gitlab.novalocal>
2020-09-25 00:03:05 +0800

    add files

--- ansible/roles/mysql-install/files/mysql-install.sh
+++ ansible/roles/mysql-install/files/mysql-install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-rpm -ivh mysql.rpm
-systemctl enable mysqld
-systemctl start mysqld
 
+##install mysql
+rpm -ivh mysql.rpm
+rm -rf /etc/my.cnf && cp /tmp/my.cnf /etc/my.cnf
+systemctl enable mysqld && systemctl start mysqld
+initpw=`grep  password /var/log/mysqld.log | awk '{print $11}'`
+mysqladmin -uroot -p$(initpw) password 'newpw'
+mysql -uroot -p'newpw' -e "grant all privileges  on *.* to 'xxx'@'10.0.1.%' identified by 'xxxx123456*' ;"
